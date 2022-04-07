852d6cebdf18b90b77ce40dc4a36ceff66a79bf1
Alexangel Sanchez <alexangel2323@gmail.com>
2022-04-06 20:17:12 -0500

    commit tree fea10ec5da12e6604711734c7734062a59d765f7

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Write your solution here!
+var cats = ["Milo", "Otis", "Garfield" ];
+
+beforeEach(function () 
+{
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+});
+
+function destructivelyAppendCat(name)
+{
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name)
+{
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat()
+{
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat()
+{
+    cats.shift();
+}
+
+function appendCat(name)
+{
+    var newArray = cats.slice();
+    newArray.push(name);
+    return newArray;
+}
+
+function prependCat(name)
+{
+    var newArray = cats.slice(0);
+    newArray.unshift(name);
+    return newArray;
+}
+
+function removeLastCat()
+{
+    var newArray = cats.slice(0,-1);
+    return newArray;
+}
+
+function removeFirstCat()
+{
+    var newArray = cats.slice(1);
+    return newArray;
+}
